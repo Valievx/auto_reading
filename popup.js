dcc0451955a2502cc89ca49c2b29c548bd2c1c32
Alexander Valiev <118052965+Valievx@users.noreply.github.com>
2023-12-20 22:40:22 +0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
-const playBtn = document.getElementById("playBtn");
-playBtn.addEventListener("click",() => {    
-    chrome.tabs.query({active: true}, (tabs) => {
-        const tab = tabs[0];
-        if (tab) {
-            alert(tab.id)
-        } else {
-            alert("There are no active tabs")
-        }
-    })
-})
+// Ждет, пока DOM-дерево полностью загрузится (событие 'DOMContentLoaded').
+// Получает ссылку на кнопку с идентификатором 'playBtn'.
+document.addEventListener('DOMContentLoaded', function() { 
+  var highlightButton = document.getElementById('playBtn');
 
+  // Устанавливает обработчик события 'click' на эту кнопку.
+  // При клике на кнопку выполняется функция, которая делает запрос к API браузера Chrome, 
+  // чтобы получить информацию о текущей вкладке (активной и в текущем окне).
+  // Затем отправляет сообщение с действием 'readText' на контент-скрипт в этой вкладке.
+  highlightButton.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: 'readText'});
+    });
+  });
+});
+ 
